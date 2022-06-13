cf3f6441b146707f098ad505fed4cdc9698cf430
Juan Cruz Saldaño <70445613+fowardelcac@users.noreply.github.com>
2022-06-12 22:11:29 -0300

    Update trade.py

--- scripts/trade.py
+++ scripts/trade.py
@@ -7,9 +7,8 @@ opcion = 0
 def main():
     c = contract_deployed()
     user = user_address()
-    wmatic = addreesses('wmatic', True)
-    dai = addreesses('dai', True)
-    fl = c.initFlashLoan(dai, wmatic, 1000000000000000000000,
+    weth, dai = setting_tokens()
+    fl = c.initFlashLoan(dai, weth, 1000000000000000000000,
                          opcion, {'from': user})
     fl.wait(1)
     print(fl.events['LogSwapper'])
